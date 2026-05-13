b4196c05612ac0b07288e65d2ea79e9a5a7f2a61
Y_Trueno <13738085852@163.com>
2026-05-13 11:29:07 +0800

    feat：适配adc、pwm、spi等外设

--- platform/mcu/YM310_OpenCPU/hal/soc/qm_adc.c
+++ platform/mcu/YM310_OpenCPU/hal/soc/qm_adc.c
@@ -1,180 +1,79 @@
 #include "qm_adc.h"
-#include "yopen_adc.h"
+#include "qm_errno.h"
+#include "hal/adc.h"
 
-#define QM_ADC_DBG(...) //yopen_trace("[qm_adc][%s][%d]"__VA_ARGS__, __FUNCTION__, __LINE__)
-
-typedef struct {
-    yopen_adc_chan_id adc_channel;
-    YOPEN_AdcAioResDiv_e resdiv;
-    int last_value;
-} qm_adc_priv_t;
+#define QM_ADC_DBG(...) //QM_LOGD("[qm_adc][%s][%d]"__VA_ARGS__, __FUNCTION__, __LINE__)
 
 int32_t qm_adc_init(qm_adc_dev_t *adc)
 {
-    qm_adc_priv_t *priv;
-
     if (adc == NULL) {
-        return -1;
+        return -QM_EINVAL;
     }
 
-    priv = (qm_adc_priv_t *)adc->priv;
-    if (priv == NULL) {
-        return -1;
-    }
-
-    switch (adc->port) {
-        case 0:
-            priv->adc_channel = YOPEN_ADC0_CHANNEL;
-            break;
-        case 1:
-            priv->adc_channel = YOPEN_ADC1_CHANNEL;
-            break;
-        case 2:
-            priv->adc_channel = YOPEN_ADC_TEM_CHANNEL;
-            break;
-        case 3:
-            priv->adc_channel = YOPEN_ADC_VBAT_CHANNEL;
-            break;
-        default:
-            return -1;
-    }
-
-    if (priv->resdiv == 0) {
-        priv->resdiv = YOPEN_ADC_AIO_RESDIV_RATIO_1;
-    }
-
-    QM_ADC_DBG("init adc port %d channel %d resdiv %d", 
-               adc->port, priv->adc_channel, priv->resdiv);
-    return 0;
+    QM_ADC_DBG("init adc port %d pin %d", adc->port, adc->pin.adc_pin);
+    return QM_EOK;
 }
 
 int32_t qm_adc_value_get(qm_adc_dev_t *adc, void *output, uint32_t timeout)
 {
-    yopen_errcode_adc_e ret;
-    qm_adc_priv_t *priv;
-    int adc_raw;
+    UINT16 value;
+
+    (void)timeout;
 
     if (adc == NULL || output == NULL) {
-        return -1;
+        return -QM_EINVAL;
     }
 
-    priv = (qm_adc_priv_t *)adc->priv;
-    if (priv == NULL) {
-        return -1;
-    }
+    QM_ADC_DBG("value_get adc port %d pin %d", adc->port, adc->pin.adc_pin);
 
-    QM_ADC_DBG("value_get adc port %d channel %d resdiv %d", 
-               adc->port, priv->adc_channel, priv->resdiv);
-
-    if (priv->adc_channel == YOPEN_ADC_VBAT_CHANNEL) {
-        YOPEN_AdcVbatResdiv_e vbat_div = (YOPEN_AdcVbatResdiv_e)priv->resdiv;
-        ret = yopen_adc_get_volt_raw(priv->adc_channel, (YOPEN_AdcAioResDiv_e)vbat_div, &adc_raw);
-        if (ret != YOPEN_ADC_SUCCESS) {
-            QM_ADC_DBG("yopen_adc_get_volt_raw failed %d", ret);
-            return -1;
-        }
-        *(int *)output = adc_raw * 32 / (8 - vbat_div);
+    if (adc->port == 3) {
+        value = hal_AdcVbatRead();
     } else {
-        ret = yopen_adc_get_volt_raw(priv->adc_channel, priv->resdiv, &adc_raw);
-        if (ret != YOPEN_ADC_SUCCESS) {
-            QM_ADC_DBG("yopen_adc_get_volt_raw failed %d", ret);
-            return -1;
-        }
-        switch (priv->resdiv) {
-            case YOPEN_ADC_AIO_RESDIV_RATIO_1:
-                *(int *)output = adc_raw;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_14OVER16:
-                *(int *)output = adc_raw * 16 / 14;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_12OVER16:
-                *(int *)output = adc_raw * 16 / 12;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_10OVER16:
-                *(int *)output = adc_raw * 16 / 10;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_8OVER16:
-                *(int *)output = adc_raw * 16 / 8;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_7OVER16:
-                *(int *)output = adc_raw * 16 / 7;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_6OVER16:
-                *(int *)output = adc_raw * 16 / 6;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_5OVER16:
-                *(int *)output = adc_raw * 16 / 5;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_4OVER16:
-                *(int *)output = adc_raw * 16 / 4;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_3OVER16:
-                *(int *)output = adc_raw * 16 / 3;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_2OVER16:
-                *(int *)output = adc_raw * 16 / 2;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_RATIO_1OVER16:
-                *(int *)output = adc_raw * 16 / 1;
-                break;
-            case YOPEN_ADC_AIO_RESDIV_BYPASS:
-                *(int *)output = adc_raw;
-                break;
-            default:
-                *(int *)output = adc_raw;
-                break;
-        }
+        value = hal_AdcRead(adc->pin.adc_pin);
     }
 
-    priv->last_value = *(int *)output;
+    *(int *)output = value;
     QM_ADC_DBG("value_get result %d", *(int *)output);
-    return 0;
+    return QM_EOK;
 }
 
 int32_t qm_adc_start(qm_adc_dev_t *adc, void *data, uint32_t size)
 {
-    qm_adc_priv_t *priv;
     int i, ret;
     int *buffer = (int *)data;
 
     if (adc == NULL || data == NULL || size == 0) {
-        return -1;
+        return -QM_EINVAL;
     }
 
-    priv = (qm_adc_priv_t *)adc->priv;
-    if (priv == NULL) {
-        return -1;
-    }
-
-    QM_ADC_DBG("start adc port %d channel %d size %d", 
-               adc->port, priv->adc_channel, size);
+    QM_ADC_DBG("start adc port %d pin %d size %d", adc->port, adc->pin.adc_pin, size);
 
     for (i = 0; i < size / sizeof(int); i++) {
         ret = qm_adc_value_get(adc, &buffer[i], 0);
-        if (ret != 0) {
-            return -1;
+        if (ret != QM_EOK) {
+            return -QM_EIO;
         }
     }
 
-    return 0;
+    return QM_EOK;
 }
 
 int32_t qm_adc_stop(qm_adc_dev_t *adc)
 {
     if (adc == NULL) {
-        return -1;
+        return -QM_EINVAL;
     }
 
-    QM_ADC_DBG("stop adc port %d", adc->port);
-    return 0;
+    QM_ADC_DBG("stop adc port %d pin %d", adc->port, adc->pin.adc_pin);
+    return QM_EOK;
 }
 
 int32_t qm_adc_deinit(qm_adc_dev_t *adc)
 {
     if (adc == NULL) {
-        return -1;
+        return -QM_EINVAL;
     }
 
-    QM_ADC_DBG("deinit adc port %d", adc->port);
-    return 0;
+    QM_ADC_DBG("deinit adc port %d pin %d", adc->port, adc->pin.adc_pin);
+    return QM_EOK;
 }
--- platform/mcu/YM310_OpenCPU/hal/soc/qm_gpio.c
+++ platform/mcu/YM310_OpenCPU/hal/soc/qm_gpio.c
@@ -1,267 +1,155 @@
 #include "qm.h"
 #include "qm_gpio.h"
-#include "qm_platform.h"
-#include "yopen_gpio.h"
-
-
-
-typedef struct{
-    yopen_GpioDir dir;
-    yopen_PullMode pull;
-}gpio_config_t;
-
-typedef struct{
-    yopen_GpioNum pin_id;
-    uint8_t func_id;
-    gpio_config_t *config;
-}gpio_list_info_t;
-
-static gpio_list_info_t g_gpio_info[200] = 
-{
-    [QM_GPIO_PIN_82] = {GPIO_0, 0, NULL},   
-    [QM_GPIO_PIN_22] = {GPIO_1, 0, NULL},   
-    [QM_GPIO_PIN_23] = {GPIO_2, 0, NULL},   
-    [QM_GPIO_PIN_54] = {GPIO_3, 0, NULL},   
-    [QM_GPIO_PIN_80] = {GPIO_4, 0, NULL},   
-    [QM_GPIO_PIN_81] = {GPIO_5, 0, NULL},   
-    [QM_GPIO_PIN_55] = {GPIO_6, 0, NULL},   
-    [QM_GPIO_PIN_56] = {GPIO_7, 0, NULL},   
-    [QM_GPIO_PIN_66] = {GPIO_8, 0, NULL},   
-    [QM_GPIO_PIN_67] = {GPIO_9, 0, NULL},   
-    [QM_GPIO_PIN_28] = {GPIO_10, 0, NULL},  
-    [QM_GPIO_PIN_29] = {GPIO_11, 0, NULL},  
-    [QM_GPIO_PIN_64] = {GPIO_12, 0, NULL},  
-    [QM_GPIO_PIN_63] = {GPIO_13, 0, NULL},  
-    [QM_GPIO_PIN_62] = {GPIO_14, 0, NULL},  
-    [QM_GPIO_PIN_49] = {GPIO_15, 0, NULL},  
-    [QM_GPIO_PIN_38] = {GPIO_16, 0, NULL},  
-    [QM_GPIO_PIN_39] = {GPIO_17, 0, NULL},  
-    [QM_GPIO_PIN_17] = {GPIO_18, 0, NULL},  
-    [QM_GPIO_PIN_18] = {GPIO_19, 0, NULL},  
-    [QM_GPIO_PIN_5] = {GPIO_20, 0, NULL},   
-    [QM_GPIO_PIN_6] = {GPIO_21, 0, NULL},   
-    [QM_GPIO_PIN_19] = {GPIO_22, 0, NULL},  
-    [QM_GPIO_PIN_100] = {GPIO_23, 0, NULL}, 
-    [QM_GPIO_PIN_101] = {GPIO_24, 0, NULL}, 
-    [QM_GPIO_PIN_16] = {GPIO_25, 0, NULL},  
-    [QM_GPIO_PIN_25] = {GPIO_26, 0, NULL},  
-    [QM_GPIO_PIN_20] = {GPIO_27, 0, NULL},  
-    // GPIO28 内部占用
-    [QM_GPIO_PIN_30] = {GPIO_29, 0, NULL},  
-    [QM_GPIO_PIN_31] = {GPIO_30, 0, NULL},  
-    [QM_GPIO_PIN_32] = {GPIO_31, 0, NULL},  
-    [QM_GPIO_PIN_33] = {GPIO_32, 0, NULL},  
-    [QM_GPIO_PIN_26] = {GPIO_33, 0, NULL},  
-    [QM_GPIO_PIN_53] = {GPIO_34, 0, NULL},  
-    [QM_GPIO_PIN_52] = {GPIO_35, 0, NULL},  
-    [QM_GPIO_PIN_78] = {GPIO_36, 0, NULL},  
-    [QM_GPIO_PIN_50] = {GPIO_37, 0, NULL},  
-    [QM_GPIO_PIN_51] = {GPIO_38, 0, NULL},  
-};
-
-
-
+#include "qm_errno.h"
+#include "hal/gpio.h"
 
 int32_t qm_gpio_init(qm_gpio_dev_t *gpio)
 {
-    int ret = 0;
-    static int init = 1;
-    yopen_LvlMode lvl_mode = LVL_LOW;
-    gpio_list_info_t *gpio_info = NULL;
+    GPIOReturnCode ret;
+    GPIOConfiguration config = {0};
 
-    if(gpio == NULL){
+    if (gpio == NULL) {
         return -QM_EINVAL;
     }
 
-    if(gpio->port >= QM_ARRAY_SIZE(g_gpio_info)){
-        return -QM_EINVAL;
-    }
-    gpio_info = &g_gpio_info[gpio->port];
-
-    gpio_config_t *gpio_config = (gpio_config_t *)qm_malloc(sizeof(gpio_config_t));
-    if(gpio_config == NULL){
-        return -QM_ENOMEM;
-    }
-    memset(gpio_config, 0, sizeof(gpio_config_t));
-
-    yopen_gpio_set_voltage(Vol_1_80V);
-
-    yopen_gpio_deinit(gpio_info->pin_id);
-	
-    if(init){
-        init = 0;
-        //使能AON POWER AON与普通GPIO电压不同，需要单独使能电源域
-	    yopen_aon_power_on();	
-    }
-
-    switch (gpio->config.mode)
-    {
+    switch (gpio->config.mode) {
         case QM_GPIO_MODE_INPUT:
-            gpio_config->dir = GPIO_INPUT;
+            config.pinDir = GPIO_IN_PIN;
             break;
         case QM_GPIO_MODE_OUTPUT:
-            gpio_config->dir = GPIO_OUTPUT;
+            config.pinDir = GPIO_OUT_PIN;
             break;
+        case QM_GPIO_MODE_OUTPUT_OD:
+            config.pinDir = GPIO_OUT_PIN;
+            break;
+        case QM_GPIO_MODE_INPUT_OUTPUT_OD:
+        case QM_GPIO_MODE_INPUT_OUTPUT:
+            config.pinDir = GPIO_OUT_PIN;
+            break;
+        case QM_GPIO_MODE_DISABLE:
         default:
-            ret = -QM_EIO;
-            break;
+            return -QM_EIO;
     }
 
-    switch (gpio->config.pull_en)
-    {
+    switch (gpio->config.pull_en) {
         case QM_GPIO_PULLUP_ONLY:
-            lvl_mode = LVL_HIGH;
-            gpio_config->pull = FORCE_PULL_UP;
+            config.pinPull = GPIO_PULLUP_ENABLE;
             break;
         case QM_GPIO_PULLDOWN_ONLY:
-            lvl_mode = LVL_LOW;
-            gpio_config->pull = FORCE_PULL_DOWN;
+            config.pinPull = GPIO_PULLDN_ENABLE;
+            break;
+        case QM_GPIO_PULLUP_PULLDOWN:
+            config.pinPull = GPIO_PULLUP_ENABLE;
             break;
         case QM_GPIO_FLOATING:
-            gpio_config->pull = FORCE_PULL_NONE;
-            break;
         default:
-            ret = -QM_EIO;
+            config.pinPull = GPIO_PULL_DISABLE;
             break;
     }
 
-    if(ret != QM_EOK){
-        qm_free(gpio_config);
-        return ret;
-    }
-
-#if 0
-    ret = yopen_pin_set_func(gpio->port, gpio_info->func_id);
-    if(ret != YOPEN_GPIO_SUCCESS){
-        qm_free(gpio_config);
+    ret = hal_ConfigGpio(gpio->port, config);
+    if (ret != GPIORC_OK) {
         return -QM_EIO;
     }
-
-    ret = yopen_gpio_init(gpio_info->pin_id, gpio_config->dir, gpio_config->pull, lvl_mode);
-    if(ret != YOPEN_GPIO_SUCCESS){
-        qm_free(gpio_config);
-        return -QM_EIO;
-    }
-#else
-    yopen_pin_set_func(gpio->port, gpio_info->func_id);
-    yopen_gpio_init(gpio_info->pin_id, gpio_config->dir, gpio_config->pull, lvl_mode);
-#endif
-
-    gpio_info->config = gpio_config;
-
-    gpio->priv = gpio_info;
 
     return QM_EOK;
 }
 
 int32_t qm_gpio_set_level(qm_gpio_dev_t *gpio, uint8_t level)
 {
-    int ret = QM_EOK;
-    yopen_LvlMode lvl_mode = LVL_LOW;
-    if (gpio == NULL || gpio->priv == NULL){
+    if (gpio == NULL) {
         return -QM_EINVAL;
     }
-    gpio_list_info_t *gpio_info = (gpio_list_info_t *)gpio->priv;
 
-    if(level){
-        lvl_mode = LVL_HIGH;
-    }else{
-        lvl_mode = LVL_LOW;
-    }
-    ret = yopen_gpio_set_level(gpio_info->pin_id, lvl_mode);
-
+    hal_GpioOut(gpio->port, level);
     return QM_EOK;
 }
 
 uint8_t qm_gpio_get_level(qm_gpio_dev_t *gpio)
 {
-    uint8_t level = 0;
-    yopen_LvlMode lvl_mode = LVL_LOW;
-    gpio_list_info_t *gpio_info = (gpio_list_info_t *)gpio->priv;
-    if (gpio == NULL || gpio->priv == NULL){
+    GPIOReturnCode ret;
+
+    if (gpio == NULL) {
         return -QM_EINVAL;
     }
 
-    yopen_gpio_get_level(gpio_info->pin_id, &lvl_mode);
-
-    if(lvl_mode == LVL_HIGH){
-        level = 1;
-    }else{
-        level = 0;
-    }
-
-    return level;
+    ret = hal_GpioIn(gpio->port);
+    return (ret == GPIORC_HIGH) ? 1 : 0;
 }
 
 int32_t qm_gpio_enable_irq(qm_gpio_dev_t *gpio, qm_gpio_intr_type_t intr_type, qm_gpio_irq_handler_t handler, void *arg)
 {
-    yopen_EdgeMode edge = EDGE_RISING;
-    yopen_TriggerMode trigger = EDGE_TRIGGER;
-    gpio_list_info_t *gpio_info = (gpio_list_info_t *)gpio->priv;
-    if (gpio == NULL || gpio->priv == NULL){
+    GPIOConfiguration config = {0};
+    GPIOReturnCode ret;
+
+    if (gpio == NULL || handler == NULL) {
         return -QM_EINVAL;
     }
-    gpio_config_t *gpio_config = (gpio_config_t *)gpio_info->config;
 
-    switch(intr_type)
-    {
-        case QM_GPIO_INTR_ANYEDGE: //下降沿
-            edge = EDGE_BOTH;
-            trigger = EDGE_TRIGGER;
-            break;
-        case QM_GPIO_INTR_POSEDGE:    //上升沿
-            edge = EDGE_RISING;
-            trigger = EDGE_TRIGGER;
-            break;
-        case QM_GPIO_INTR_NEGEDGE: //下降沿
-            edge = EDGE_FALLING;
-            trigger = EDGE_TRIGGER;
-            break;
-        case QM_GPIO_INTR_LOW_LEVEL: //低电平
-            edge = EDGE_RISING;
-            trigger = LEVEL_TRIGGER;
-            break;
-        case QM_GPIO_INTR_HIGH_LEVEL: //高电平
-            edge = EDGE_FALLING;
-            trigger = LEVEL_TRIGGER;
-            break;
+    config.pinDir = GPIO_IN_PIN;
+    config.isr = (GPIOCallback)handler;
 
+    switch (intr_type) {
+        case QM_GPIO_INTR_POSEDGE:
+            config.pinEd = GPIO_RISE_EDGE;
+            break;
+        case QM_GPIO_INTR_NEGEDGE:
+            config.pinEd = GPIO_FALL_EDGE;
+            break;
+        case QM_GPIO_INTR_ANYEDGE:
+            config.pinEd = GPIO_TWO_EDGE;
+            break;
+        case QM_GPIO_INTR_LOW_LEVEL:
+        case QM_GPIO_INTR_HIGH_LEVEL:
+            config.pinEd = GPIO_TWO_EDGE;
+            break;
+        case QM_GPIO_INTR_DISABLE:
         default:
-            
+            return -QM_EINVAL;
+    }
+
+    switch (gpio->config.pull_en) {
+        case QM_GPIO_PULLUP_ONLY:
+            config.pinPull = GPIO_PULLUP_ENABLE;
+            break;
+        case QM_GPIO_PULLDOWN_ONLY:
+            config.pinPull = GPIO_PULLDN_ENABLE;
+            break;
+        case QM_GPIO_PULLUP_PULLDOWN:
+            config.pinPull = GPIO_PULLUP_ENABLE;
+            break;
+        case QM_GPIO_FLOATING:
+        default:
+            config.pinPull = GPIO_PULL_DISABLE;
             break;
     }
 
-    
-    /*
-    * MAIN_RI 引脚的中断回调中，获取中断引脚电平，并发送消息到消息队列处理任务中
-    * 
-    * Notes: 中断回调中不可执行耗时较长的操作，不可调用阻塞性接口，否则会引起系统异常。
-    */
-    yopen_int_register(gpio_info->pin_id, trigger, DEBOUNCE_DIS, edge, gpio_config->pull, handler, arg);
-    
-    yopen_int_enable(gpio_info->pin_id);
+    ret = hal_ConfigGpio(gpio->port, config);
+    if (ret != GPIORC_OK) {
+        return -QM_EIO;
+    }
+
     return QM_EOK;
 }
 
 int32_t qm_gpio_disable_irq(qm_gpio_dev_t *gpio)
 {
-    gpio_list_info_t *gpio_info = (gpio_list_info_t *)gpio->priv;
-    if (gpio == NULL || gpio->priv == NULL){
+    if (gpio == NULL) {
         return -QM_EINVAL;
     }
-    gpio_config_t *gpio_config = (gpio_config_t *)gpio_info->config;
 
-    yopen_int_disable(gpio_info->pin_id);
-
+    hal_GpioDisableEdgeDet(gpio->port, GPIO_TWO_EDGE);
     return QM_EOK;
 }
 
 int32_t qm_gpio_clear_irq(qm_gpio_dev_t *gpio)
 {
+    (void)gpio;
     return QM_EOK;
 }
 
 int32_t qm_gpio_deinit(qm_gpio_dev_t *gpio)
 {
+    (void)gpio;
     return QM_EOK;
 }
